f1675530420047c063ee357bd9624772d1591e59
Jheimys Bassani <jheimysbb@gmail.com>
2026-04-17 15:53:41 +0100

    Criando a pasta de crendenciais dentro de fixture

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,16 +3,20 @@ describe('Login', () => {
      cy.visit('http://localhost:4000')
   })
   it('Login com dados válidos deve permitir acesso ao sistema', () => {
-   
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('123456')
+    cy.fixture('credenciais').then((credenciais) => {
+      cy.get('#username').click().type(credenciais.valida.username)
+      cy.get('#senha').click().type(credenciais.valida.senha)
+
+    })
     cy.contains('button', 'Entrar').click()
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
 
    it('Login com dados inválidos deve apresentar mensagem de erro', () => {
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('1234567')
+    cy.fixture('credenciais').then((credenciais) => {
+      cy.get('#username').click().type(credenciais.invalida.username)
+      cy.get('#senha').click().type(credenciais.invalida.senha)
+    })
     cy.contains('button', 'Entrar').click()
     cy.get('.toast')
 
